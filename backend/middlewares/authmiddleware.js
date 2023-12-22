fc2d8802882f290d48ac3ae28f1309fcb5538d5d
starringRashmi <purkayastharashmi@gmail.com>
2023-12-22 23:57:44 +0530

    auth updated

--- backend/middlewares/authmiddleware.js
+++ backend/middlewares/authmiddleware.js
@@ -2,31 +2,27 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const protect = async (req, res, next) => {
-    let token;
-    if (
-        req.headers.authorization && req.headers.authorization.startsWith("Bearer")
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
 
-    ) {
-        try {
-            token = req.headers.authorization.split(" ")[1];
+      //decodes token id sends it without password
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
 
-            //decodes token id sends it without password
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select("-password");
-
-            next();
-
-        } catch (error) {
-            res.status(401);
-            console.log("not authorized,no token"); 
-
-        }
-
+      next();
+    } catch (error) {
+      res.status(401);
+      console.log("not authorized,no token");
     }
-    if (!token) {
-        res.status(401);
-        console.log("not authorised ,no token");
-
-    }
-    }
-module.exports={protect};
+  }
+  if (!token) {
+    res.status(401);
+    console.log("not authorised ,no token");
+  }
+};
+module.exports = { protect };
